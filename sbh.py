5d5c90f7289dd82fa470b456116f09100463f3c1
Bambo <kyrstke@gmail.com>
2022-05-29 21:59:29 +0200

    Debug, first version working

--- sbh.py
+++ sbh.py
@@ -13,7 +13,7 @@ class sbhAlgorithm():
     
 
     def __euclidean__(self, a, b):
-        return math.sqrt(pow(a[1] - b[1], 2) + pow(a[0] - b[0], 2))
+        return math.sqrt(pow(int(a[1]) - int(b[1]), 2) + pow(int(a[0]) - int(b[0]), 2))
 
 
     def __match__(self, v1, v2):
@@ -49,12 +49,18 @@ class sbhAlgorithm():
 
     def antColonySearch(self):
         self.readfile(sys.argv[1])
-        world = pants.World(self.vertices, self.__euclidean__)
+        world = pants.World(self.vertices, self.__match__) # self.__euclidean__
         solver = pants.Solver()
+
+        # solution = solver.solve(world)
         solutions = solver.solutions(world)
-        print(solutions.distance)
-        print(solutions.tour)
-        print(solutions.path)
+
+        for solution in solutions:
+            print(f'Distance: {solution.distance}')
+            print(f'Tour: {solution.tour}')
+            print('Path:')
+            for i in solution.path:
+                print(f'Start: {i.start}, end: {i.end}, length: {i.length}, pheromone: {i.pheromone}')
 
 
     def main(self):
